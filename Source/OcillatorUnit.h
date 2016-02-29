a79db1735cb4fe9bf7517bd5f233a58faa02ff95
Mikael <mikael.ohlsson92@gmail.com>
2016-02-29 12:45:27 +0000

    Fixed OcillatorUnit.h compilation errors

--- Source/OcillatorUnit.h
+++ Source/OcillatorUnit.h
@@ -13,53 +13,32 @@
 #include "Phasor.h"
 
 class OcillatorUnit : public Phasor {
+public: enum Wave { SINE };
+    
 private:
     float *m_waveTable;
     int m_waveTableSize;
     int m_index = 0;
-//    typedef Phasor super;
+    OcillatorUnit::Wave m_type = SINE;
     
 public:
-    enum Wave { SINE };
-    
-    OcillatorUnit (float samplerate, int  frequency, enum type, int size):
-        Phasor(samplerate, frequency),
-        m_type(type),
-        m_waveTableSize(size)
-    {
-        setType(type);
-    }
-    
-    OcillatorUnit (float samplerate, int  frequency, enum type):
-        m_phasor(samplerate, frequency),
-        m_type(type),
-        m_waveTableSize(512)
-    {
-        setType(type);
-    }
-    
-    OcillatorUnit(float samplerate, int  frequency):
-        m_phasor(samplerate, frequency),
-        m_type(SINE),
-        m_waveTableSize(512)
-    {
-        setType(SINE);
-    };
-    
-    void setType (enum type) {
-        if (type != m_type) {
-            switch (type) {
-                case SINE:
-                    fillWaveTable(&std:sin);
-                default:
-                    break;
-            }
-        }
-    }
     
     void fillWaveTable (float (*f) (float)) {
         for (int i = 0; i < m_waveTableSize; i++) {
             m_waveTable[i] = f(double_Pi * (i/m_waveTableSize));
+        }
+    }
+    
+    void setType (OcillatorUnit::Wave type) {
+        if (type != m_type) {
+            m_type = type;
+            
+            switch (type) {
+                case SINE:
+                    fillWaveTable(&std::sin);
+                default:
+                    break;
+            }
         }
     }
     
@@ -68,11 +47,31 @@ public:
     }
     
     
-    void tick override() {
+    void tick() override {
         Phasor::tick();
         m_index = (int) (Phasor::getPhase() * m_waveTableSize);
     }
     
+    OcillatorUnit (float samplerate, int  frequency, Wave type, int size):
+        Phasor(samplerate, frequency),
+        m_waveTableSize(size)
+    {
+        setType(type);
+    }
+    
+    OcillatorUnit (float samplerate, int  frequency, Wave type):
+        Phasor(samplerate, frequency),
+        m_waveTableSize(512)
+    {
+        setType(type);
+    }
+    
+    OcillatorUnit(float samplerate, int  frequency):
+        Phasor(samplerate, frequency),
+        m_waveTableSize(512)
+    {
+        setType(SINE);
+    }
 };
 
 #endif /* defined(__APDelay__OcillatorUnit__) */
